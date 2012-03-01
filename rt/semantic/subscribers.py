1c018dcd71388cdfa3b541314335ff9f508ec35a
Luca <lucabel@gmail.com>
2012-03-01 17:48:56 +0100

    fix problem checking IText interface on field

--- rt/semantic/subscribers.py
+++ rt/semantic/subscribers.py
@@ -7,9 +7,9 @@ def set_semantic_data(obj, event):
     text = []
     for schema in iterSchemata(obj):
         fields = getFieldsInOrder(schema)
-        for field in fields:
+        for name, field in fields:
             if IText.providedBy(field):
                 text.append(field.get(obj))
     full_text = "\n".join(text)
     pos_tags = tag(full_text)
-    return extract_keywords(pos_tags)
+    obj.semantic = extract_keywords(pos_tags)
