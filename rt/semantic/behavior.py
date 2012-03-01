50abc3a0b850f772ff29df2a84b4321fcce1e6aa
Yiorgis Gozadinos <ggozad@jarn.com>
2012-03-01 13:03:27 +0100

    Order of imports, whitespace

--- rt/semantic/behavior.py
+++ rt/semantic/behavior.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
-from plone.app.dexterity.behaviors.metadata import DCFieldProperty, MetadataBase
-from plone.autoform.interfaces import IFormFieldProvider
-from plone.directives import form
-from rt.semantic import semanticMessageFactory as _
-from z3c.form.interfaces import IEditForm, IAddForm, IDisplayForm
 from zope import schema
 from zope.interface.declarations import alsoProvides
 
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.directives import form
+from z3c.form.interfaces import IEditForm, IDisplayForm
+
+from rt.semantic import semanticMessageFactory as _
+
 
 class ISemantic(form.Schema):
-    """ 
+    """
     Semantic behavior interface
     """
     semantic = schema.Tuple(
-        title = _("label_semantic", default=u"Semantic"),
-        description = _("help_semantic", default=u""),
+        title=_("label_semantic", default=u"Semantic"),
+        description=_("help_semantic", default=u""),
         required=False,
-        value_type = schema.TextLine(),
-        missing_value = (),
+        value_type=schema.TextLine(),
+        missing_value=(),
     )
     form.omitted('semantic')
     form.no_omit(IDisplayForm, 'semantic')
     form.no_omit(IEditForm, 'semantic')
-    #form.no_omit(IAddForm, 'semantic')
-    
+
 alsoProvides(ISemantic, IFormFieldProvider)
