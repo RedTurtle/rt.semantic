7b2d9ca8b42c6b4c3ec709aa8e7c87d7ab071bdd
Yiorgis Gozadinos <ggozad@jarn.com>
2012-03-01 10:40:31 +0100

    Clean up and pyflakes

--- rt/semantic/tagger.py
+++ rt/semantic/tagger.py
@@ -1,4 +1,8 @@
 import nltk
 
+
 def tag(text):
+    """
+    Tokenizes and POS-tags text.
+    """
     return nltk.pos_tag(nltk.word_tokenize(text))
--- rt/semantic/tests/test_pos_tagger.py
+++ rt/semantic/tests/test_pos_tagger.py
@@ -1,21 +1,16 @@
 import unittest
 
-from Products.CMFCore.utils import getToolByName
-
-from rt.semantic.testing import\
-    REDTURTLE_SEMANTIC_INTEGRATION_TESTING
-
 from rt.semantic.tagger import tag
+
 
 class TestPOSTagger(unittest.TestCase):
 
-#    layer = REDTURTLE_SEMANTIC_INTEGRATION_TESTING
-
-    the_sentence = 'The quick brown fox jumps over the lazy dog'
-
     def test_pos_tag(self):
-        tagged = tag(self.the_sentence)
-        self.assertEqual([('The', 'DT'), ('quick', 'NN'), ('brown', 'NN'), 
-                          ('fox', 'NN'), ('jumps', 'NNS'), ('over', 'IN'), 
+        sentence = 'The quick brown fox jumps over the lazy dog'
+        tagged = tag(sentence)
+        self.assertEqual([('The', 'DT'), ('quick', 'NN'), ('brown', 'NN'),
+                          ('fox', 'NN'), ('jumps', 'NNS'), ('over', 'IN'),
                           ('the', 'DT'), ('lazy', 'NN'), ('dog', 'NN')], tagged)
-        
+
+    def test_empty_text(self):
+        self.assertEqual([], tag(''))
