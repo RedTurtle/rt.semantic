01c378c8ddb5b5d80069f99a20adb2e2498f8de8
Luca <lucabel@gmail.com>
2012-03-01 09:57:03 +0100

    add tagtext module and write down the test

--- rt/semantic/tests/test_pos_tagger.py
+++ rt/semantic/tests/test_pos_tagger.py
@@ -1,14 +1,23 @@
 import unittest
+import nltk
 
 from Products.CMFCore.utils import getToolByName
 
 from rt.semantic.testing import\
     REDTURTLE_SEMANTIC_INTEGRATION_TESTING
 
+from rt.semantic.tagtext import tag_this_text 
 
 class TestPOSTagger(unittest.TestCase):
 
 #    layer = REDTURTLE_SEMANTIC_INTEGRATION_TESTING
 
+    the_sentence = 'The quick brown fox jumps over the lazy dog'
+
     def test_pos_tag(self):
-        pass
+        text = nltk.word_tokenize(self.the_sentence)
+        tagged = tag_this_text(text)
+        self.assertEqual([('The', 'DT'), ('quick', 'NN'), ('brown', 'NN'), 
+                          ('fox', 'NN'), ('jumps', 'NNS'), ('over', 'IN'), 
+                          ('the', 'DT'), ('lazy', 'NN'), ('dog', 'NN')], tagged)
+        
