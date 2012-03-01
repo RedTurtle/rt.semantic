2cd2ceef7deb3b75eaadc5a688d1ebded7b729a0
Luca <lucabel@gmail.com>
2012-03-01 17:32:27 +0100

    Add extract_keyword function and test on it

--- rt/semantic/subscribers.py
+++ rt/semantic/subscribers.py
@@ -1,6 +1,7 @@
 from zope.schema import getFieldsInOrder
 from zope.schema.interfaces import IText
 from plone.dexterity.utils import iterSchemata
+from rt.semantic.tagger import tag, extract_keywords
 
 def set_semantic_data(obj, event):
     text = []
@@ -10,3 +11,5 @@ def set_semantic_data(obj, event):
             if IText.providedBy(field):
                 text.append(field.get(obj))
     full_text = "\n".join(text)
+    pos_tags = tag(full_text)
+    return extract_keywords(pos_tags)
--- rt/semantic/tests/test_pos_tagger.py
+++ rt/semantic/tests/test_pos_tagger.py
@@ -1,9 +1,15 @@
 import unittest
+from os.path import dirname, join
 
-from rt.semantic.tagger import tag
-
+from rt.semantic.tagger import tag, extract_keywords
+import rt.semantic.tests
 
 class TestPOSTagger(unittest.TestCase):
+
+    def setUp(self):
+        path = join(dirname(rt.semantic.tests.__file__), 'data', 'alicereview.txt')
+        with open(path,'r') as fd:
+            self.alicetext = fd.read()
 
     def test_pos_tag(self):
         sentence = 'The quick brown fox jumps over the lazy dog'
@@ -14,3 +20,12 @@ class TestPOSTagger(unittest.TestCase):
 
     def test_empty_text(self):
         self.assertEqual([], tag(''))
+
+    def test_extract_keywords(self):
+        pos_tags = tag(self.alicetext)
+        most_important = extract_keywords(pos_tags)
+        self.assertEqual(most_important,
+                         ['door', 'table', 'house', 'notices', 'time', 'baby', 
+                          'day', 'finds', 'head', 'mushroom'])
+        
+        
