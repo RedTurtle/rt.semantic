0bc5b465f1d9064669af53cce2083cfd5992b9c3
Mirco Angelini <mirco.angelini@redturtle.it>
2012-03-01 16:32:45 +0100

    calculate text field for subscriber

--- rt/semantic/subscribers.py
+++ rt/semantic/subscribers.py
@@ -1,3 +1,12 @@
+from zope.schema import getFieldsInOrder
+from zope.schema.interfaces import IText
+from plone.dexterity.utils import iterSchemata
 
 def set_semantic_data(obj, event):
-    pass
+    text = []
+    for schema in iterSchemata(obj):
+        fields = getFieldsInOrder(schema)
+        for field in fields:
+            if IText.providedBy(field):
+                text.append(field.get(obj))
+    full_text = "\n".join(text)
